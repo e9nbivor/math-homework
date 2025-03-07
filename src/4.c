730122bcd118851afee944315fbf44e7ade2ef51
e9nbivor <e9nbivor@gmx.com>
2025-03-07 03:56:23 +0000

    Add 4.c

---
+++ src/4.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+  int a = 5;
+  int b = 2;
+  int c = 0;
+
+  c = a + b;
+
+  printf("The sum of %d and %d is %d", a, b, c);
+
+  return 0;
+}
